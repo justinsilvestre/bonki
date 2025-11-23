92da59432d2640fa5af9bb8787b6093b7da67649
Justin Silvestre <justinsilvestre@gmail.com>
2025-11-23 16:28:54 +0100

    Make character scene with color parameters

---
+++ bonki_appearance_parameters.gd
@@ -0,0 +1,11 @@
+extends RefCounted
+
+class_name BonkiAppearanceParameters
+
+@export var body_color: Color = Color.CADET_BLUE
+@export var eye_shine_color: Color = Color.AQUAMARINE
+@export var eye_shadow_color: Color = Color.YELLOW
+@export var eye_base_color: Color = Color.DARK_CYAN
+
+@export var skull_stretch_factor: float = 1
+@export var spine_base_spread_factor: float = 1

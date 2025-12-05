2000f282b0bd6d6c5de0ac9dc408fb58b9934762
Justin Silvestre <justinsilvestre@gmail.com>
2025-12-05 17:11:48 +0100

    Get bonki blend shapes + animations working

--- bonki_appearance_parameters.gd
+++ bonki_appearance_parameters.gd
@@ -7,5 +7,9 @@ class_name BonkiAppearanceParameters
 @export var eye_shadow_color: Color = Color.YELLOW
 @export var eye_base_color: Color = Color.DARK_CYAN
 
-@export var skull_stretch_factor: float = 1
-@export var spine_base_spread_factor: float = 1
+@export var wide_stretch_factor: float = 0
+@export var horn_stretch_factor: float = 0
+@export var long_stretch_factor: float = 0
+@export var pearness_factor: float = 0
+@export var tall_stretch_factor: float = 0
+@export var wonkiness_factor: float = 0
